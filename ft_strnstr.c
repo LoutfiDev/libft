62d483816abc5bd329f6f7c44e7cae7602b45d44
Yahya Loutfi <yloutfi@e2r7p1.1337.ma>
2022-10-09 12:49:24 +0100

    strnstr is done

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,32 @@
 /*   By: yloutfi <soulang.dev@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 11:14:21 by yloutfi           #+#    #+#             */
-/*   Updated: 2022/10/09 11:14:21 by yloutfi          ###   ########.fr       */
+/*   Updated: 2022/10/09 12:48:46 by yloutfi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char *ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	size_t	lenght;
+	char *str;
+
+	i = 0;
+	j = 0;
+	lenght = ft_strlen(needle);
+	str = (char *)haystack;
+	if (lenght == 0)
+		return (str);
+	while (str[i] && i < len)
+	{
+		if (str[i] == needle[j])
+			j++;
+		else
+			j = 0;
+		i++;
+		if (j == lenght)
+			return (str + i - j);
+	}
+	return (0);   
+}
